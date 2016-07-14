484250b83d07f546400aad4d7e739931828ba476
Sebastian Valdez <valdez.sebastian4@gmail.com>
2016-07-14 00:49:24 -0700

    Add columns

--- db/migrate/20160707020903_create_meetings.rb
+++ db/migrate/20160707020903_create_meetings.rb
@@ -2,11 +2,18 @@ class CreateMeetings < ActiveRecord::Migration
   def change
     create_table :meetings do |t|
       t.integer :user_id
+      t.string :uuid
       t.string :host_email
+      t.string :id_of_meeting
+      t.string :type
       t.datetime :start_time
       t.datetime :end_time
-      t.string :uuid
       t.integer :participant_count
+
+      t.string :duration
+      t.boolean :has_pstn
+      t.boolean :has_voip
+      t.boolean :has_screen_share, :default => false
 
     end
     add_index :meetings, :host_email
