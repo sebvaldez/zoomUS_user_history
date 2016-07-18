a8e4dc7d46559124227b6e4f9086547b89a96672
Sebastian Valdez <valdez.sebastian4@gmail.com>
2016-07-18 15:19:01 -0700

    set :default_env for rbenv

--- config/deploy.rb
+++ config/deploy.rb
@@ -34,7 +34,10 @@ set :linked_files, %w{config/database.yml config/secrets.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # Default value for default_env is {}
-set :default_env, { path: "/opt/ruby/bin:$PATH" }
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Set env to use output of 'which bundler'
+set :default_env, { path: "/home/deploy/.rbenv/shims:~/.rbenv/bin:$PATH" }
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
